1c8e1bacd0a25a8b43dadbd57ac48ee25b40e705
EcoRanger <adiputrachristian17@gmail.com>
2021-07-28 00:10:50 +0700

    feat: create CheckinObservation view
    note: add a dummy image in Preview Asset

---
+++ emocean/Resources/CheckinObservation/CheckinObservationView.swift
@@ -0,0 +1,63 @@
+//
+//  CheckinObservationView.swift
+//  emocean
+//
+//  Created by Christian Adiputra on 27/07/21.
+//
+
+import SwiftUI
+
+struct CheckinObservationView: View {
+    
+    // MARK: PROPERTIES
+    @Binding var question: String
+    
+    
+    // MARK: BODY
+    var body: some View {
+        
+        ZStack { // START: ZTACK
+            
+            Image("dummy1")
+                .resizable()
+                .ignoresSafeArea()
+            
+            
+            GeometryReader { geo in // START: GEOMETRYREADER
+                
+                VStack(alignment: .center) { // START: VSTACK
+                    
+                    Spacer()
+                    
+                    Text(question)
+                        .padding()
+                        .font(.title)
+                        .lineLimit(nil)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    HStack { // START: HSTACK
+                        
+                        PrimaryButton(content: {
+                            Text("No")
+                        }, maxWidth: 65, action: {})
+                        
+                        PrimaryButton(content: {
+                            Text("Yes")
+                        }, maxWidth: 65, action: {})
+
+                    }.padding(.bottom,27) // END: HSTACK
+                } // END: VSTACK
+                .frame(width: geo.size.width , height: geo.size.height, alignment: .center)
+            } // END: GEOMETRYREADER
+        } // END: ZTACK
+    } 
+}
+
+struct CheckinObservationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckinObservationView(question: .constant("Do you wanna talk about it?"))
+    }
+}
