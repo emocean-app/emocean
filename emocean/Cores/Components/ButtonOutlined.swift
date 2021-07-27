02193ce072bb25c88e3f74045763e397197718c8
Puras Handharmahua <48229056+handharr@users.noreply.github.com>
2021-07-27 22:23:23 +0700

    feat: create button outlined (#4)

---
+++ emocean/Cores/Components/ButtonOutlined.swift
@@ -0,0 +1,106 @@
+//
+//  ButtonOutlined.swift
+//  emocean
+//
+//  Created by Puras Handharmahua on 27/07/21.
+//
+
+import SwiftUI
+
+/// A Button with outline around it.
+///
+/// This button has two theme, light and dark.
+/// light theme will give the button white outline and white text color, while dark theme is black
+///
+/// ```
+/// ButtonOutlined(
+///     text: "Title",
+///     isSelected: $selected,
+///     isLight: true,
+///     action: {
+///         print("ButtonOutlined Clicked")
+///     }
+/// )
+/// ```
+///
+/// - Parameter text: The title for button
+/// - Parameter isSelected: a binding Boolean
+/// - Parameter isLight: Boolean, wheter the button outlined is light or dark theme
+/// - Parameter action: a void closure, an action for ButtonOutlined
+///
+struct ButtonOutlined: View {
+    
+    // MARK: PROPERTIES
+    var text: String
+    @Binding var isSelected: Bool
+    var isLight: Bool = true
+    var action: (() -> Void?)? = nil
+    
+    private let whiteOpacity = Color.white.opacity(0.5)
+    private let whiteColor = Color.white
+    private let blackColor = Color.black
+    private let clearColor = Color.clear
+    
+    // MARK: BODY
+    var body: some View {
+        Button(action: { // START: BUTTON
+            action?()
+        }, label: {
+            Text(text)
+                .foregroundColor(isLight ? .white : .black)
+                .padding()
+                .padding(.horizontal)
+                .background( // START: BACKGROUND
+                    ZStack { // START: ZTACK
+                        backgroundLayer
+                            .clipShape(Capsule())
+                        Capsule()
+                            .stroke(
+                                strokeLayer,
+                                style: StrokeStyle(
+                                    lineWidth: 2
+                                )
+                            )
+                    } // END: ZTACK
+                ) // END: BACKGROUND
+        }) // END: BUTTON
+    }
+}
+
+// MARK: - COMPONENTS
+extension ButtonOutlined {
+    var backgroundLayer: Color {
+        if isSelected {
+            return
+                whiteOpacity
+        } else {
+            return clearColor
+        }
+    }
+    
+    var strokeLayer: Color {
+        if isLight && isSelected {
+            return clearColor
+        } else if isLight && !isSelected {
+            return whiteColor
+        } else {
+            return blackColor
+        }
+    }
+}
+
+// MARK: PREVIEW
+struct ButtonOutlined_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonOutlined(
+            text: "Covid",
+            isSelected: .constant(false),
+            action: {
+                print("Outlined Button Clicked")
+            }
+        )
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.purple)
+    }
+}
