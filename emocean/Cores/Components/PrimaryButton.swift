4b01468e9c0e548583276a1d04131344e5fb0e41
Farrel Najib Anshary <farrelnajib@gmail.com>
2021-07-27 23:42:53 +0700

    feat: create primary button (#5)

---
+++ emocean/Cores/Components/PrimaryButton.swift
@@ -0,0 +1,73 @@
+//
+//  PrimaryButton.swift
+//  emocean
+//
+//  Created by Farrel Anshary on 27/07/21.
+//
+
+import SwiftUI
+
+/// A solid button with primary color accent.
+///
+/// You can customize the maxWidth of this button if you need. Keep it empty to make it follows the text length.
+///
+/// ```
+/// PrimaryButton(
+///     content: {
+///         HStack {
+///             Image(systemName: "arrow.down.to.line.alt")
+///             Text("Click Me!")
+///         }
+///     },
+///     maxWidth: .infinity,
+///     action: {
+///         print("PrimaryButton Clicked")
+///     }
+/// )
+/// ```
+///
+/// - Parameter content: The content for the button (should be inside HStack if you have multiple views)
+/// - Parameter maxWidth: The width of the button
+/// - Parameter action: a void closure, an action for ButtonOutlined
+///
+struct PrimaryButton<Content: View>: View {
+    // MARK: PROPERTIES
+    let content: Content
+    let maxWidth: CGFloat?
+    let action: () -> Void
+    
+    // MARK: INITIALIZER
+    init(@ViewBuilder content: () -> Content, maxWidth: CGFloat? = nil, action: @escaping () -> Void) {
+        self.content = content()
+        self.maxWidth = maxWidth
+        self.action = action
+    }
+    
+    // MARK: BODY
+    var body: some View {
+        Button(action: action, label: { // START: BUTTON
+            content
+                .foregroundColor(.white)
+                .frame(maxWidth: maxWidth)
+                .padding()
+                .padding(.horizontal, 20)
+                .background(Color(red: 16/255, green: 28/255, blue: 56/255))
+                .clipShape(Capsule())
+        }) // END: BUTTON
+    }
+}
+
+// MARK: PREVIEW
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton(content: {
+            HStack {
+                Image(systemName: "arrow.down.to.line.alt")
+                Text("Click Me!")
+            }
+        }, maxWidth: 150) {
+            print("Primary Button Clicked")
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
