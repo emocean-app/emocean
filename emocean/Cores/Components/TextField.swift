99d609ef8d45a4769e17f6ae290b8e499ac78fd4
Wilson Adrilia <56957857+wilsonadrilia@users.noreply.github.com>
2021-07-29 22:49:28 +0700

    TextView singleview (#20)

---
+++ emocean/Cores/Components/TextField.swift
@@ -0,0 +1,138 @@
+//
+//  TextField.swift
+//  emocean
+//
+//  Created by Wilson Adrilia on 29/07/2021.
+//
+
+import SwiftUI
+
+fileprivate struct UITextViewWrapper: UIViewRepresentable {
+    typealias UIViewType = UITextView
+
+    @Binding var text: String
+    @Binding var calculatedHeight: CGFloat
+    var onDone: (() -> Void)?
+
+    func makeUIView(context: UIViewRepresentableContext<UITextViewWrapper>) -> UITextView {
+        let textField = UITextView()
+        textField.delegate = context.coordinator
+
+        textField.isEditable = true
+        textField.font = UIFont.preferredFont(forTextStyle: .body)
+        textField.isSelectable = true
+        textField.isUserInteractionEnabled = true
+        textField.isScrollEnabled = true
+        textField.backgroundColor =  UIColor(white: 1.0, alpha: 0.75)
+        if nil != onDone {
+            textField.returnKeyType = .done
+        }
+
+        textField.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextView, context: UIViewRepresentableContext<UITextViewWrapper>) {
+        if uiView.text != self.text {
+            uiView.text = self.text
+        }
+        if uiView.window != nil, !uiView.isFirstResponder {
+            uiView.becomeFirstResponder()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(text: $text, height: $calculatedHeight, onDone: onDone)
+    }
+
+    final class Coordinator: NSObject, UITextViewDelegate {
+        var text: Binding<String>
+        var calculatedHeight: Binding<CGFloat>
+        var onDone: (() -> Void)?
+
+        init(text: Binding<String>, height: Binding<CGFloat>, onDone: (() -> Void)? = nil) {
+            self.text = text
+            self.calculatedHeight = height
+            self.onDone = onDone
+        }
+
+        func textViewDidChange(_ uiView: UITextView) {
+            text.wrappedValue = uiView.text
+//            UITextViewWrapper.recalculateHeight(view: uiView, result: calculatedHeight)
+        }
+
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            if let onDone = self.onDone, text == "\n" {
+                textView.resignFirstResponder()
+                onDone()
+                return false
+            }
+            return true
+        }
+    }
+
+}
+
+struct MultilineTextField: View {
+
+    private var placeholder: String
+    private var onCommit: (() -> Void)?
+
+    @Binding private var text: String
+    private var internalText: Binding<String> {
+        Binding<String>(get: {self.text}) {
+            self.text = $0
+            self.showingPlaceholder = $0.isEmpty
+        }
+    }
+
+    @State private var dynamicHeight: CGFloat = 340
+    @State private var showingPlaceholder = false
+
+    init (_ placeholder: String = "", text: Binding<String>, onCommit: (() -> Void)? = nil) {
+        self.placeholder = placeholder
+        self.onCommit = onCommit
+        self._text = text
+        self._showingPlaceholder = State<Bool>(initialValue: self.text.isEmpty)
+    }
+
+    var body: some View {
+        UITextViewWrapper(text: self.internalText, calculatedHeight: $dynamicHeight, onDone: onCommit)
+            .frame(minHeight: dynamicHeight, maxHeight: dynamicHeight)
+            .background(placeholderView, alignment: .topLeading)
+            .cornerRadius(16)
+    }
+
+    var placeholderView: some View {
+        Group {
+            if showingPlaceholder {
+                Text(placeholder).foregroundColor(.gray)
+                    .padding(.leading, 4)
+                    .padding(.top, 8)
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct MultilineTextField_Previews: PreviewProvider {
+    static var test:String = ""
+//  some very very very long description string to be initially wider than screen"
+    
+    static var testBinding = Binding<String>(get: { test }, set: {
+        test = $0 })
+
+    static var previews: some View {
+        VStack(alignment: .leading) {
+//            Text("Description:")
+            MultilineTextField(text: testBinding, onCommit: {
+                print("Final text: \(test)")
+            })
+            Spacer()
+        }
+        .padding()
+        .background(Color.theme.sunsetBottomGradient)
+        .previewLayout(.fixed(width: 360, height: 365))
+    }
+}
+#endif
