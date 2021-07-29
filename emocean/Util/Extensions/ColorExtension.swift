4718860d12a1fe07bfe238eacf9e2163ca6ca095
Puras Handharmahua <48229056+handharr@users.noreply.github.com>
2021-07-29 12:13:53 +0700

    Add extension for gradient and color

---
+++ emocean/Util/Extensions/ColorExtension.swift
@@ -0,0 +1,32 @@
+//
+//  ColorExtension.swift
+//  emocean
+//
+//  Created by Puras Handharmahua on 29/07/21.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color {
+    static let theme = EMColorTheme()
+}
+
+struct EMColorTheme {
+    let blueQuadrant = Color("BlueQuadrant")
+    let greenQuadrant = Color("GreenQuadrant")
+    let morningTopGradient = Color("MorningTopGradient")
+    let morningBottomGradient = Color("MorningBottomGradient")
+    let nightTopGradient = Color("NightTopGradient")
+    let nightBottomGradient = Color("NightBottomGradient")
+    let sunsetTopGradient = Color("SunsetTopGradient")
+    let sunsetBottomGradient = Color("SunsetBottomGradient")
+    let primary = Color("Primary")
+    let grayPrimary = Color("PrimaryGray")
+    let redQuadrant = Color("RedQuadrant")
+    let seaBottomGradient = Color("SeaBottomGradient")
+    let seaTopGradient = Color("SeaTopGradient")
+    let graySecondary = Color("SecondaryGray")
+    let grayThird = Color("ThirdGray")
+    let yellowQuadrant = Color("YellowQuadrant")
+}
