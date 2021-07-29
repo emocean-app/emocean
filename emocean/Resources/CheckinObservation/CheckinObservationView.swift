87561be9f98223193ccee1f26834e12f41d402e6
EcoRanger <adiputrachristian17@gmail.com>
2021-07-29 23:53:48 +0700

    func: create tab bar

--- emocean/Resources/CheckinObservation/CheckinObservationView.swift
+++ emocean/Resources/CheckinObservation/CheckinObservationView.swift
@@ -14,46 +14,35 @@
 import SwiftUI
 
 struct CheckinObservationView: View {
-    
     // MARK: PROPERTIES
     @Binding var question: String
-    
-    
     // MARK: BODY
     var body: some View {
-        
         ZStack { // START: ZTACK
-            
             Image("dummy1") /// - Background Image
                 .resizable()
                 .ignoresSafeArea()
-                
                 VStack(alignment: .center) { // START: VSTACK
-                    
                     Spacer()
-                    
                     Text(question)
                         .padding()
                         .font(.title)
                         .lineLimit(nil)
                         .multilineTextAlignment(.center)
                         .foregroundColor(.white)
-                    
                     Spacer()
                     
                     HStack { // START: HSTACK
-                        
                         PrimaryButton(content: {
                             Text("No")
                         }, maxWidth: 65, action: {})
-                        
                         PrimaryButton(content: {
                             Text("Yes")
                         }, maxWidth: 65, action: {})
                     }.padding(.bottom, 30) // END: HSTACK
                 } // END: VSTACK
         } // END: ZTACK
-    } 
+    }
 }
 
 struct CheckinObservationView_Previews: PreviewProvider {
