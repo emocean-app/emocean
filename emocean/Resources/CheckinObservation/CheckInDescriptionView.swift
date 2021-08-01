d425a3a2a6865abbd61929c9bee69fc95ea37ea6
Wilson Adrilia <wilsonadrilia@gmail.com>
2021-08-01 16:22:44 +0700

    CheckInDesc View

---
+++ emocean/Resources/CheckinObservation/CheckInDescriptionView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckInDescriptionView.swift
+//  emocean
+//
+//  Created by Wilson Adrilia on 01/08/2021.
+//
+
+import SwiftUI
+
+struct CheckInDescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckInDescriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckInDescriptionView()
+    }
+}
