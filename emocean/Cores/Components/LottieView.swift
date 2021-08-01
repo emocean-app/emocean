07ee23a4f1c57b01facfcf5784959cc789de0618
Wilson Adrilia <wilsonadrilia@gmail.com>
2021-08-01 22:51:42 +0700

    Lottie Setup

---
+++ emocean/Cores/Components/LottieView.swift
@@ -0,0 +1,20 @@
+//
+//  LottieView.swift
+//  emocean
+//
+//  Created by Wilson Adrilia on 01/08/2021.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LottieView_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieView()
+    }
+}
