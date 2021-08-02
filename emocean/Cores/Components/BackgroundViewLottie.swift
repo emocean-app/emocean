31b22d24b436fd35c194164b51a610193f676e81
Wilson Adrilia <56957857+wilsonadrilia@users.noreply.github.com>
2021-08-02 10:30:42 +0700

    Lottie Package + Lottie View Test + CheckinDescView (#28)

---
+++ emocean/Cores/Components/BackgroundViewLottie.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundViewLottie.swift
+//  emocean
+//
+//  Created by Wilson Adrilia on 31/08/2021.
+//
+
+import SwiftUI
+
+struct BackgroundViewLottie: View {
+    var body: some View {
+        LottieView(filename: "AnimasiHomepageSore")
+    }
+}
+
+struct BackgroundViewLottie_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundViewLottie()
+    }
+}
+
