665fae9e11b03375db728b438d387e0ecc295b05
adiputrachristian <71424536+adiputrachristian@users.noreply.github.com>
2021-08-06 21:01:39 +0700

    func: update gone fishing (#57)
    * func: create-gone-fishing-view
    
    * func-update-went-fishing
    
    * func-update-gone-fishing

--- emocean/Cores/Components/WentFishingView.swift
+++ emocean/Cores/Components/WentFishingView.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct WentFishingView: View {
+    let time = Time()
     var body: some View {
         ZStack{
             EMTheme.shared.sea.ignoresSafeArea()
-            VStack (alignment: .center, spacing: 0) {
+            VStack (alignment: .center, spacing: -10) {
                 Spacer()
-                LottieView(filename: "GambarGoneFishing", contentMode: .scaleToFill)
+                LottieView(filename: "GoneFishing\(time.getRawValue())", contentMode: .scaleToFill)
                     .frame(width: 250, height: 250, alignment: .center)
                 Text("Gone Fishing!")
                     .font(.title)
                     .fontWeight(.semibold)
                     .foregroundColor(.white)
-                    .padding(.bottom, 3)
+                    .padding(.bottom, 13)
                 Text("This page will be available soon")
                     .foregroundColor(.white)
                 Spacer()
