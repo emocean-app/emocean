1bb670f5cf0e7a9c79d2c604cf65d34fc67c7a2a
EcoRanger <adiputrachristian17@gmail.com>
2021-08-09 19:02:03 +0700

    func: create-sound-player

---
+++ emocean/Cores/Components/SoundManager.swift
@@ -0,0 +1,35 @@
+//
+//  SoundPlayer.swift
+//  emocean
+//
+//  Created by Christian Adiputra on 09/08/21.
+//
+
+import Foundation
+import AVKit
+
+class SoundManager {
+    static let shared = SoundManager()
+    var player: AVAudioPlayer?
+    
+    enum SoundOption: String {
+        case ocean
+        case backsong
+        case water
+    }
+    
+    func playSound(sound: SoundOption) {
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("Error playing sound")
+        }
+    }
+    
+    func stopSound(sound: SoundOption){
+        guard let player = player else { return }
+        player.stop()
+    }
+}
