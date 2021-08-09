4eb5a8b9c7cef49e12c17acc26d3ff4d005a1890
Farrel Najib Anshary <farrelnajib@gmail.com>
2021-08-09 09:52:42 +0700

    feat: apply settings globally (#61)
    - Benerin spacing tulisan "Ahoy"
    - Ilangin commented codes

--- emocean/Cores/Components/MainHeaderView.swift
+++ emocean/Cores/Components/MainHeaderView.swift
@@ -12,6 +12,8 @@ struct MainHeaderView: View {
     @Binding var shouldPopUpFullScreen: Bool
     @Binding var shouldShowSettingsModal: Bool
     let time = Time()
+    
+    @EnvironmentObject var settingsEnv: SettingsViewModel
 
     var body: some View {
         GeometryReader { reader in
@@ -25,26 +27,60 @@ struct MainHeaderView: View {
                     .aspectRatio(contentMode: .fill)
                     .offset(y: -getOffset(reader, multiplier: 3) + 15)
                     .frame(width: reader.size.width, height: getOffset(reader, multiplier: 3) + 480)
-                    .modifier(ParallaxMotionModifier(manager: manager, magnitude: 20, orientation: .vertical))
+                    .modifier(ParallaxMotionModifier(manager: manager, magnitude: !settingsEnv.reduceMotion ? 20 : 0, orientation: .vertical))
                 
                 Image("Boat")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .offset(y: -getOffset(reader, multiplier: 10) + 40)
-                    .frame(width: reader.size.width, height: getOffset(reader, multiplier: 8) + 480)
-                    .modifier(ParallaxMotionModifier(manager: manager, magnitude: 30, orientation: .vertical))
+                    .offset(y: -getOffset(reader, multiplier: 12) + 40)
+                    .frame(width: reader.size.width, height: getOffset(reader, multiplier: 12) + 480)
+                    .modifier(ParallaxMotionModifier(manager: manager, magnitude: !settingsEnv.reduceMotion ? 30 : 0, orientation: .vertical))
 
-                VStack(alignment: .leading) {
-                    Spacer()
-                        .frame(height: 75)
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading) {
+                        Spacer()
+                            .frame(height: 75)
 
-                    HStack {
                         Text("Ahoy!")
                             .font(.largeTitle)
                             .bold()
                             .foregroundColor(time.timeRange == .night ? .white : Color.theme.primary)
                             .frame(maxWidth: .infinity, alignment: .leading)
-                        
+
+                        Text("Do you wanna check in?")
+                            .font(.title3)
+                            .foregroundColor(time.timeRange == .night ? .white : Color.theme.primary)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+
+                        if time.timeRange == .night {
+                            GrayButton {
+                                HStack {
+                                    Image(systemName: "lifepreserver")
+                                    Text("Let's Dive In!")
+                                        .bold()
+                                }
+                                .padding(.horizontal, 5)
+                            } action: {
+                                self.shouldPopUpFullScreen = true
+                            }
+                        } else {
+                            PrimaryButton {
+                                HStack {
+                                    Image(systemName: "lifepreserver")
+                                    Text("Let's Dive In!")
+                                        .bold()
+                                }
+                                .padding(.horizontal, 5)
+                            } action: {
+                                self.shouldPopUpFullScreen = true
+                            }
+                        }
+                        Spacer()
+                    }
+
+                    VStack {
+                        Spacer()
+                            .frame(height: 75)
                         Button(action: {
                             self.shouldShowSettingsModal = true
                         }, label: {
@@ -54,37 +90,6 @@ struct MainHeaderView: View {
                                 .foregroundColor(time.timeRange == .night ? .white : Color.theme.primary)
                         })
                     }
-
-                    Text("Do you wanna check in?")
-                        .font(.title3)
-                        .foregroundColor(time.timeRange == .night ? .white : Color.theme.primary)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-
-                    if time.timeRange == .night {
-                        GrayButton {
-                            HStack {
-                                Image(systemName: "lifepreserver")
-                                Text("Let's Dive In!")
-                                    .bold()
-                            }
-                            .padding(.horizontal, 5)
-                        } action: {
-                            self.shouldPopUpFullScreen = true
-                        }
-                    } else {
-                        PrimaryButton {
-                            HStack {
-                                Image(systemName: "lifepreserver")
-                                Text("Let's Dive In!")
-                                    .bold()
-                            }
-                            .padding(.horizontal, 5)
-                        } action: {
-                            self.shouldPopUpFullScreen = true
-                        }
-                    }
-
-                    Spacer()
                 }
                 .padding(.horizontal, 30)
             }
