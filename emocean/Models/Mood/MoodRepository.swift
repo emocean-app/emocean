acbbfb406aecf6d4240bbe0f0daf7a13323e7071
Puras Handharmahua <48229056+handharr@users.noreply.github.com>
2021-08-16 11:13:02 +0700

    feat: checkin (#78)
    * update checkin
    
    Refactor folder, update some view and fix some animation
    
    * update checkin feature
    
    - update checkin flow
    - fix transition from observation to description
    - add animation to description
    
    * Update CheckinSuccessView.swift
    
    * update checkin
    
    * update checkin
    
    - change API baseURL
    - configure decoding
    - fix some bugs
    
    * update checkin
    
    * update checkin

--- emocean/Models/Mood/MoodRepository.swift
+++ emocean/Models/Mood/MoodRepository.swift
@@ -9,22 +9,21 @@ import Foundation
 import Combine
 
 struct MoodRepository {
+    // TYPE
     typealias Entity = Mood
-    
+    // PROPERTIES
     private var staticStore = SeederStaticMood()
     private var networkStore = SeederNetworkingMood()
     private var cancellables = Set<AnyCancellable>()
-    
+    // METHODS
     func getAllData() -> AnyPublisher<[Entity], NetworkRequestError> {
-
         return networkStore
             .getAllMoods()
             .map({ data -> [Entity] in
-                return data.data
+                return data.moods
             })
             .eraseToAnyPublisher()
     }
-    
     func getAllDummy() -> [Entity] {
         return staticStore.getData()
     }
